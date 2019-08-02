504abb73ed02c45f6e2948e0a3c465553391ed84
Will Hodgson <will.hodgson@current-rms.com>
2019-08-02 15:17:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
 
   def self.new_from_filename(filename)
     delimiters = ['.', '-']
-    a = self.split(Regexp.union(delimiters))
+    a = filename.split(Regexp.union(delimiters))
   end
 
 end
